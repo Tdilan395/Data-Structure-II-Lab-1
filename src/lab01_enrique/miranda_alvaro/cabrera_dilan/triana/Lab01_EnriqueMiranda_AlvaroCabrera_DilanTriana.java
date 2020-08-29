0d246dfb40732a7b6af2abdd11ab00fd4e8596fd
Dilan <tdilan@uninorte.edu.co>
2020-08-28 19:55:10 -0500

    Real Initial commit

---
+++ src/lab01_enrique/miranda_alvaro/cabrera_dilan/triana/Lab01_EnriqueMiranda_AlvaroCabrera_DilanTriana.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab01_enrique.miranda_alvaro.cabrera_dilan.triana;
+
+/**
+ *
+ * @author Domain
+ */
+public class Lab01_EnriqueMiranda_AlvaroCabrera_DilanTriana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Lets do this shit");
+    }
+    
+}
