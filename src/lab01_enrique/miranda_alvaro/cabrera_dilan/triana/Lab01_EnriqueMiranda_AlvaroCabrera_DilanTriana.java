e220754cbb7df8d2c3732ee346731562254b0f4d
Alvaro <calvaroj@uninorte.edu.co>
2020-08-30 18:43:52 -0500

    Separar done.
    Pedal puro.

--- src/lab01_enrique/miranda_alvaro/cabrera_dilan/triana/Lab01_EnriqueMiranda_AlvaroCabrera_DilanTriana.java
+++ src/lab01_enrique/miranda_alvaro/cabrera_dilan/triana/Lab01_EnriqueMiranda_AlvaroCabrera_DilanTriana.java
@@ -5,6 +5,10 @@
  */
 package lab01_enrique.miranda_alvaro.cabrera_dilan.triana;
 
+import java.util.ArrayList;
+import static lab01_enrique.miranda_alvaro.cabrera_dilan.triana.Test1.deArchivoALista;
+import org.json.JSONObject;
+
 /**
  *
  * @author Domain
@@ -15,7 +19,35 @@ public class Lab01_EnriqueMiranda_AlvaroCabrera_DilanTriana {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Lets do this shit");
+        ArrayList<JSONObject> posts = Test1.deArchivoALista(1, 6, "posts");
+        for (JSONObject post : posts) {
+            System.out.println(post.get("id"));
+        }
+        ArrayList<JSONObject> comentarios=Test1.deArchivoALista(1, 7, "comments");
+        for (JSONObject comentario : comentarios) {
+            System.out.println(comentario.get("id"));
+        }
+        ArrayList<JSONObject> usuarios=Test1.deArchivoALista(1, 23, "usuario");
+         ArrayList<JSONObject> addresses= new ArrayList();
+         ArrayList<JSONObject> geos= new ArrayList();
+         ArrayList<JSONObject> companies= new ArrayList();
+         int c=0;
+        for (JSONObject usuario : usuarios) {
+            System.out.println(usuario.get("id"));
+            addresses.add(new JSONObject(usuario.get("address").toString()));
+            geos.add(new JSONObject(addresses.get(c).get("geo").toString()));
+            companies.add(new JSONObject(usuario.get("company").toString()));
+            c++;
+        }
+        for (JSONObject address : addresses) {
+            System.out.println(address.get("street"));
+        }
+        for (JSONObject geo : geos) {
+            System.out.println(geo);
+        }
+        for (JSONObject company : companies) {
+            System.out.println(company.get("name"));
+        }
     }
-    
+
 }
--- src/lab01_enrique/miranda_alvaro/cabrera_dilan/triana/Test1.java
+++ src/lab01_enrique/miranda_alvaro/cabrera_dilan/triana/Test1.java
@@ -23,85 +23,69 @@ import org.json.JSONObject;
  */
 public class Test1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        ArrayList<JSONObject> objetos = deArchivoALista();
-       
-        System.out.println(objetos.get(0).get("userId"));
-    
-    }
-
-     public static ArrayList<JSONObject> deArchivoALista() {
+    public static ArrayList<JSONObject> deArchivoALista(int lim1, int lim2, String ruta) {
         ArrayList<JSONObject> objetos = new ArrayList();
-        
-        File f = new File("pedidos.txt");
-      
+
+        File f = new File(ruta + ".txt");
+
         try {
             BufferedReader br = new BufferedReader(new FileReader(f));
             System.out.println("sí pasé");
-            int cont=0;
+            int cont = 0;
             String linea;
-            String object="{";
-            while((linea = br.readLine()) != null) {
-                if(cont>1 && cont<6){
-                    object+=linea;
+            String object = "{";
+            while ((linea = br.readLine()) != null) {
+                if (cont > lim1 && cont < lim2) {
+                    object += linea;
                 }
                 cont++;
-                
-                /*String cliente = datos[0];
-                String repartidor = datos[1];
-                String direccion = datos[2];
-                String comida = datos[3];
-                String costo = datos[4];*/
-                if(cont==7){
-                    System.out.println(object);
-                JSONObject wey = new JSONObject(object+"}");
-                
-                objetos.add(wey);
+                if (cont == lim2) {
+                    JSONObject wey = new JSONObject(object + "}");
+                    objetos.add(wey);
+                    object = "{";
+                    cont = 0;
                 }
-                
             }
             return objetos;
-            
+
         } catch (FileNotFoundException ex) {
-                        System.out.println("sí pasé FILO NOT");
+            System.out.println("sí pasé FILO NOT");
 
             return objetos;
         } catch (IOException ex) {
-                        System.out.println("sí pasé IOEX");
+            System.out.println("sí pasé IOEX");
 
             return objetos;
         }
-     }
-     public static ArrayList<String> separar(String a){
+    }
+
+    public static ArrayList<String> separar(String a) {
         ArrayList<String> textos = new ArrayList<>();
         int base = 0;
         for (int i = 2; i < a.length(); i++) {
-            if(equi(a.substring(base, i))==0 && "},".equals(a.substring(i-2,i))){
-                if(textos.size() == 0){
-                    textos.add(a.substring(1, i-1));
-                    base = i+1;
-                }else{
-                    textos.add(a.substring(base, i-1));
-                    base = i+1;
+            if (equi(a.substring(base, i)) == 0 && "},".equals(a.substring(i - 2, i))) {
+                if (textos.size() == 0) {
+                    textos.add(a.substring(1, i - 1));
+                    base = i + 1;
+                } else {
+                    textos.add(a.substring(base, i - 1));
+                    base = i + 1;
                 }
             }
         }
         return textos;
     }
-    
-    public static int equi(String a){
-        int x=0;
-            for (int i = 0; i < a.length(); i++) {
-                if(a.charAt(i)=='{'){
-                    x++;
-                }
-                if(a.charAt(i)=='}'){
-                    x--;
-                }
+
+    public static int equi(String a) {
+        int x = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == '{') {
+                x++;
             }
+            if (a.charAt(i) == '}') {
+                x--;
+            }
+        }
         return x;
     }
 }
