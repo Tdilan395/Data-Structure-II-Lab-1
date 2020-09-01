62dcbcf9feb51142bb60bbafa21b883985457f58
Alvaro <calvaroj@uninorte.edu.co>
2020-08-31 19:12:12 -0500

    Intento de agregar
    Intento de agredar 2.0

--- src/lab01_enrique/miranda_alvaro/cabrera_dilan/triana/Test1.java
+++ src/lab01_enrique/miranda_alvaro/cabrera_dilan/triana/Test1.java
@@ -11,39 +11,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.TreeMap;
-import javax.swing.JOptionPane;
 import org.json.JSONObject;
 
 /**
  *
  * @author user
  */
-
-/* "id": 1,
-"name": "Leanne Graham",
-"username": "Bret",
-"email": "Sincere@april.biz",
-"address": {
-"street": "Kulas Light",
-"suite": "Apt. 556",
-"city": "Gwenborough",
-"zipcode": "92998-3874",
-"geo": {
-"lat": "-37.3159",
-"lng": "81.1496"
-}
-},
-"phone": "1-770-736-8031 x56442",
-"website": "hildegard.org",
-"company": {
-"name": "Romaguera-Crona",
-"catchPhrase": "Multi-layered client-server neural-net",
-"bs": "harness real-time e-markets"
-}
-},*/
 public class Test1 {
 
     public static ArrayList<JSONObject> deArchivoALista(int lim1, int lim2, String ruta) {
@@ -81,24 +54,64 @@ public class Test1 {
             return objetos;
         }
     }
-    
-    public static User deJSONaUser(JSONObject ob){
-    JSONObject address= new JSONObject(ob.get("address").toString());
-    JSONObject company=new JSONObject(ob.get("company").toString());
-    JSONObject geo= new JSONObject(address.get("geo").toString());
-    float ge[]= {geo.getFloat("lat"),geo.getFloat("lng")};
-    Address a= new Address(address.getString("street"), address.getString("suite"), address.getString("city"),address.getString("zipcode") , ge);
-    Company c= new Company(company.getString("name"), company.getString("catchPhrase"), company.getString("bs"));
-    return new User();
-    } 
-    
-    public static Comment deJSONaComment(JSONObject ob){
+
+    public static User deJSONaUser(JSONObject ob) {
+        JSONObject address = new JSONObject(ob.get("address").toString());
+        JSONObject company = new JSONObject(ob.get("company").toString());
+        JSONObject geo = new JSONObject(address.get("geo").toString());
+        float ge[] = {geo.getFloat("lat"), geo.getFloat("lng")};
+        Address a = new Address(address.getString("street"), address.getString("suite"), address.getString("city"), address.getString("zipcode"), ge);
+        Company c = new Company(company.getString("name"), company.getString("catchPhrase"), company.getString("bs"));
+        return new User(ob.getInt("id"), null, null);
+    }
+
+    public static Comment deJSONaComment(JSONObject ob) {
         //id o postID?
         return new Comment(ob.getString("name"), ob.getString("email"), ob.getString("body"), ob.getInt("id"), null, null);
     }
-    
-    public static Post deJSONaPost(JSONObject ob){
+
+    public static Post deJSONaPost(JSONObject ob) {
         //id o UserId?
         return new Post(ob.getString("title"), ob.getString("body"), ob.getInt("id"), null, null);
+    }
+
+    public void Agregar(int nivel, Nodo Raiz) {
+        switch (nivel) {
+            case 1:
+                ArrayList<JSONObject> usuarios = Test1.deArchivoALista(1, 23, "usuario");
+                User a;
+                for (JSONObject usuario : usuarios) {
+                    a = deJSONaUser(usuario);
+//                    Nodo.insertar(a);
+//                    Agregar(2,a);
+                }
+                break;
+            case 2:
+                ArrayList<JSONObject> posts = Test1.deArchivoALista(1, 6, "posts");
+                Post p;
+                for (JSONObject post : posts) {
+                    p = deJSONaPost(post);
+//                    if(p.getUserId()==Nodo.getId()){
+//                    Nodo.insertar(p);
+//                    Agregar(3,p);
+//                    }
+//                    if(!p.listIsEmpthy() && p.getUserId()!=Nodo.getId()){
+//                        break;
+//                    }
+                }
+                break;
+            case 3:
+                ArrayList<JSONObject> comentarios = Test1.deArchivoALista(1, 7, "comments");
+                Comment c;
+                for (JSONObject comentario : comentarios) {
+                    c = deJSONaComment(comentario);
+//                  if(Nodo.getId()==c.postID()){
+//                    Nodo.insertar(c);
+//                    }
+                }
+                break;
+            default:
+                System.out.println("Solo existen 3 niveles. ");
+        }
     }
 }
